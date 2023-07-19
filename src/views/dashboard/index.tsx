37558e65ffc0fc253fddb3ebc181e6dfa5dda0b7
Jomari Iringan <jom.iringan@gmail.com>
2023-07-19 12:51:14 +0800

    chore: setup project structure & flashlist

---
+++ src/views/dashboard/index.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+// @ts-nocheck
+
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  Image,
+} from 'react-native';
+import { Box, Text } from 'react-native-design-utility';
+
+export default function Dashboard(): JSX.Element {
+
+  return (
+    <React.Fragment>      
+    </React.Fragment>
+  );
+}
